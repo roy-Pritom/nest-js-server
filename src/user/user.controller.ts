a80bbf69b85e7a9a7be03c5ecb4fc972e14e29c8
mahmud <syedmahamudulha@gmail.com>
2024-11-10 20:01:09 +0600

    add code

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,35 +1,73 @@
-import { Controller, Post, Get, Param, Body, Put } from '@nestjs/common';
+import { Controller, Post, Get, Param, Body, Put, HttpCode } from '@nestjs/common';
 import { UserService } from './user.service';
 import { CreateUserDto, UpdateUserDto } from './interfaces/user.interface';
 
-@Controller('users')
+@Controller('/api/v1/users')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
   // Create User
   @Post()
+  @HttpCode(201) // Returns a 201 status code for successful creation
   async createUser(@Body() createUserDto: CreateUserDto) {
-    return this.userService.createUser(createUserDto);
+    const createdUser = await this.userService.createUser(createUserDto);
+    return {
+      statusCode: 201,
+      message: 'User created successfully',
+      data: createdUser,
+    };
   }
 
   // Get All Users
   @Get()
+  @HttpCode(200) // Returns a 200 status code for a successful get request
   async getAllUsers() {
-    return this.userService.getAllUsers();
+    const users = await this.userService.getAllUsers();
+    return {
+      statusCode: 200,
+      message: 'Users retrieved successfully',
+      data: users,
+    };
   }
 
   // Get User by ID
   @Get(':id')
+  @HttpCode(200) // Returns a 200 status code for a successful get request
   async getUserById(@Param('id') id: number) {
-    return this.userService.getUserById(id);
+    const user = await this.userService.getUserById(Number(id));
+    if (!user) {
+      return {
+        statusCode: 404,
+        message: 'User not found',
+        data: null,
+      };
+    }
+    return {
+      statusCode: 200,
+      message: 'User retrieved successfully',
+      data: user,
+    };
   }
 
   // Update User Name
   @Put(':id')
+  @HttpCode(200) // Returns a 200 status code for successful update
   async updateUserName(
     @Param('id') id: number,
     @Body() updateUserDto: UpdateUserDto,
   ) {
-    return this.userService.updateUserName(id, updateUserDto);
+    const updatedUser = await this.userService.updateUserName(id, updateUserDto);
+    if (!updatedUser) {
+      return {
+        statusCode: 404,
+        message: 'User not found or update failed',
+        data: null,
+      };
+    }
+    return {
+      statusCode: 200,
+      message: 'User updated successfully',
+      data: updatedUser,
+    };
   }
 }
